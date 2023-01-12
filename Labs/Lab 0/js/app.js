6ebb9ea668984e6cae22be6a6b5b31fbe202b110
MHeggy <mhegedui@iu.edu>
2023-01-12 12:53:43 -0500

    Adding base lab files and structure

---
+++ Labs/Lab 0/js/app.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(800, 600);
+    background(212, 30, 42);
+
+    fill(252, 45, 187)
+    rect( 40,50, 100, 20);
+}
+
+function draw() {
+    circle(mouseX, mouseY, 12);
+
+}
