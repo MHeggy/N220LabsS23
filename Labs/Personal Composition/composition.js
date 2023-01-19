ea42adc83b588b8141f4608ab61d4722577d7e59
MHeggy <mhegedui@iu.edu>
2023-01-19 15:10:31 -0500

    Changed JS for Composition and HTML

--- Labs/Personal Composition/composition.js
+++ Labs/Personal Composition/composition.js
@@ -1,16 +1,24 @@
+//This variable represents the desired Radius of the circle used in Composition
 let r = 25;
-
+//These Variables represent the two sides of the rectangle used in Composition
+let xSide = 65;
+let ySide = 25;
 //Making a Canvas for the Composition
 function setup() {
     createCanvas(1000,800)
-    background('blue')
+    background('black')
 }
-//Drawing a circle for the first object of Composition
-function drawCircle() {
+//Drawing a basic shape by combining rectangle, circles, and triangle
+function draw() {
+    //Two circles to denote the wheels of the car
     circle(100, 250, r)
     circle(125, 250, r)
-}
-
-function drawRectangle() {
-    rect(100, 100, 25, 65)
+    //Rectangle drawn to represent the body of the car
+    rect(80, 210, xSide, ySide)
+    //Triangle drawn to represent 'Cabin' of the car
+    triangle(130, 210, 140, 150, 100, 210)
+    //Make the shapes a yellow color
+    fill(255, 204, 124)
+    //Circle drawn to represent a sun in the top left-hand corner of the Canvas
+    circle(900, 10, 250)
 }
