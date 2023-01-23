bedef12200c01d9e5329c012798d277671b501d4
MHeggy <mhegedui@iu.edu>
2023-01-22 23:51:15 -0500

    JS Update

--- Labs/Lab 2/javascript/animation.js
+++ Labs/Lab 2/javascript/animation.js
@@ -1,3 +1,13 @@
 function setup() {
     createCanvas(400, 300)
 }
+function draw() {
+    circle(mouseX, mouseY, 50px)
+    if (mouseX > 200) {
+    fill(#FF0000)
+    }
+    if (mouseX < 200) {
+    fill(#0000FF)
+    }
+    }
+    
