e661bc9e967348c42688ae30502e2ce61e925205
MHeggy <mhegedui@iu.edu>
2023-01-26 11:19:39 -0500

    ballbounce.html added and js

--- Labs/Lab 2/javascript/ballbounce.js
+++ Labs/Lab 2/javascript/ballbounce.js
@@ -1,13 +1,20 @@
 //Setting up all necessary variables first.
 let r = 50;
-var x = 5;
-var y = -5;
+var x = 0;
+var y = 0;
+var speedX = 5;
+var speedY = 5;
 //Setting up the 800 x 600 px canvas!
 function setup() {
     createCanvas(800, 600);
-    background('black');
 }
 //Now setting up the function to draw circle and have it bounce off walls.
 function draw() {
-    circle(700,300, r);
+    background('black');
+    circle(x+700, y+300, r);
+    x = x-speedX;
+    y = y+speedY;
+    if(y > height) {
+    y *= -1;
+    }
 }
