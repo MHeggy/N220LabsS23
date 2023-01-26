61b81f955c4db4a4b1df92d57f8384579b801c9b
MHeggy <mhegedui@iu.edu>
2023-01-26 16:44:33 -0500

    Changes to index.html and ballbounce js

--- Labs/Lab 2/html/javascript/ballbounce.js
+++ Labs/Lab 2/html/javascript/ballbounce.js
@@ -1,20 +1,36 @@
 //Setting up all necessary variables first.
-let r = 50;
 var x = 0;
 var y = 0;
+var width = 800;
+var height = 600;
 var speedX = 5;
 var speedY = 5;
+var directionX = -1;
+var directionY = 1;
 //Setting up the 800 x 600 px canvas!
 function setup() {
     createCanvas(800, 600);
 }
 //Now setting up the function to draw circle and have it bounce off walls.
 function draw() {
-    background('black');
-    circle(x+700, y+300, r);
-    x = x-speedX;
-    y = y+speedY;
-    if(y > height) {
-    y *= -1;
+    background('black')
+    //Making the ball
+    circle(x, y, 40)
+    //Motion
+    x = x + (directionX * speedX)
+    y = y + (directionY * speedY)
+    //If statements to change direction
+    if(x < 0) {
+        directionX = directionX*-1;
     }
+    if (y < 0) {
+        directionY = directionY*-1;
+    }
+    if(x > width) {
+        directionX = directionX*-1;
+    }
+    if(y > height) {
+        directionY = directionY*-1;
+    }
+
 }
