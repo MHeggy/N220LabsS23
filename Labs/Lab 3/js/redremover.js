c85918500b176007478a3dabd1c8327353825618
MHeggy <mhegedui@iu.edu>
2023-01-31 17:38:20 -0500

    Changes to redremover.js and html

--- Labs/Lab 3/js/redremover.js
+++ Labs/Lab 3/js/redremover.js
@@ -1,1 +1,19 @@
-let myColor = color(170, 200, 150);
+//Setting up color variable and no red variables.
+let myColor;
+let newColor;
+//Setting up canvas and black background color.
+function setup() {
+    createCanvas(800, 600);
+    background('black');
+    myColor = fill(170, 200, 150);
+}
+function draw() {
+    circle(400, 300, 50);
+    fill(0, 200, 150);
+    let newColor = noRed(myColor);
+}
+//Setting up noRed Function
+function noRed() {
+    circle(400, 300, 50);
+    myColor.setRed(0);
+}
