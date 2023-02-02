080183070771ffb63df9d310cda1bfda6e2d29ed
MHeggy <122556107+MHeggy@users.noreply.github.com>
2023-02-02 13:18:15 -0500

    polarpoint.js big changes

--- Labs/Lab 3/js/polarpoint.js
+++ Labs/Lab 3/js/polarpoint.js
@@ -1,3 +1,21 @@
-let r = 50;
+//Initializing my variables for radius, x, and y.
+let r = 10;
 let x = r * Math.sin(mouseX);
 let y = r * Math.cos(mouseX);
+let res = {}
+//Setting up the Canvas and Background for my circle to be drawn.
+function setup() {
+    createCanvas(800, 600);
+    background('black');
+}
+//Translate lines set up before the draw functions.
+translate(100,00);
+
+//Setting up the function draw().
+function draw() {
+
+}
+//Setting up the polarPoint() function.
+function polarPoint() {
+    
+}
