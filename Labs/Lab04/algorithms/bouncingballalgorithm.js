292c60a70799a4a19758573ac61aaddcff0b6c31
MHeggy <mhegedui@iu.edu>
2023-02-03 17:45:23 -0500

    Added the bouncing ball algorithm and data driven algorithm started as well.

---
+++ Labs/Lab04/algorithms/bouncingballalgorithm.js
@@ -0,0 +1,2 @@
+//We must create two separate objects each with the listed parameters of 'x, y, width and height'.
+//
---
+++ Labs/Lab04/algorithms/datadrivenalgorithm.js
@@ -0,0 +1,23 @@
+//So for this lab I must draw anything to the screen that I see fit.
+//But, the important parameter of this is the object and the associated parameters that will come along with the object.
+//Rather than assigning number values to the properties like width, height, and length within the function. I must rather assign these as a parameter to an object that I will then use to draw onto the screen.
+//I can expect the code to start with setting up the object and the associated parameters, that I have not yet decided on. But, there are plenty of things that I can do here and the freedom that has been provided to me in this assignment makes me excited!
+//For this assignment, I'm going to draw a person who is simply present within a setting where there will be a road and a person that is walking on said road.
+//Code start: 
+//Let person = {width: 80px, height: 100px, weight: 150, posx: 250, posy: 300}
+//Let road = {width: 150px, height: 20px, num_lines: 4}
+//function setup() {
+    //createCanvas(1000,700)
+//}
+//function draw() {
+    //drawPerson(
+        //drawRoad()
+    //)
+//}
+//function drawPerson() {
+    //use parameters to setup the composition.
+//}
+//function drawRoad() {
+    //use the paraneters once again to draw the composition.
+//}
+//Hardest part will probably be setting up the functions to draw the person and the road, but even harder will probably be combining the two with the correct parameters so that they mesh well together.
