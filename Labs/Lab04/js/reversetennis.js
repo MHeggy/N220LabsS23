5f095eb7701c9725f6b6eded8091dee61d29018f
MHeggy <mhegedui@iu.edu>
2023-02-03 21:09:01 -0500

    reversetennis.js updated

---
+++ Labs/Lab04/js/reversetennis.js
@@ -0,0 +1,10 @@
+//Initializing my objects with their parameters.
+let object_1 = {x: 40, y: 100, width: 40, height: 70, color: 'red'};
+let object_2 = {x: 760, y: 100, width: 40, height: 70, color: 'red'};
+//Initializing my canvas parameters and variables.
+let canvas_2 = {x: 800, y: 600}
+//Function setup section.
+function setup() {
+    createCanvas(canvas_2.x, canvas_2.y);
+    fill('black');
+}
