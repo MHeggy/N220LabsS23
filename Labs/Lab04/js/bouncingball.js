460cd3267b1f6eed76a47f9efba82e7875820b87
MHeggy <mhegedui@iu.edu>
2023-02-03 20:59:47 -0500

    bouncingball.js completed and updated

--- Labs/Lab04/js/bouncingball.js
+++ Labs/Lab04/js/bouncingball.js
@@ -3,14 +3,14 @@ let ball = {x: 0, y: 0, radius: 50, speedX: 5, speedY: 5, directionX: -1, direct
 let canvas_1 = {x: 800, y: 600}
 //Setting up the 800 x 600 px canvas!
 function setup() {
-    createCanvas(canvas_1);
+    createCanvas(canvas_1.x, canvas_1.y);
 }
 //Now setting up the function to draw circle and have it bounce off walls.
 function draw() {
     background('black')
     //Making the ball
     circle(ball.x, ball.y, ball.radius)
-    Motion
+    //Motion
     ball.x = ball.x + (ball.directionX * ball.speedX)
     ball.y = ball.y + (ball.directionY * ball.speedY)
     //If statements to change direction
@@ -20,10 +20,10 @@ function draw() {
     if (ball.y < 0) {
         ball.directionY = ball.directionY*-1;
     }
-    if(ball.x > width) {
+    if(ball.x > canvas_1.x) {
         ball.directionX = ball.directionX*-1;
     }
-    if(ball.y > height) {
+    if(ball.y > canvas_1.y) {
         ball.directionY = ball.directionY*-1;
     }
 }
