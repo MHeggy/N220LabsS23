c8aab95cea320656ad036e48215a8792275e32b9
MHeggy <mhegedui@iu.edu>
2023-02-04 15:03:37 -0500

    Links added to index.html and changes to reversetennis.js

--- Labs/Lab04/js/reversetennis.js
+++ Labs/Lab04/js/reversetennis.js
@@ -1,6 +1,6 @@
 //Initializing my objects with their parameters.
-let object_1 = {x: 40, y: 100, width: 40, height: 70, color: 'red', directionX: -1, directionY: 1, speedX: 1, speedY: 3};
-let object_2 = {x: 760, y: 100, width: 40, height: 70, color: 'red', directionX: 1, directionY: -1, speedX: 1, speedY: 3};
+let object_1 = {x: 0, y: 0, width: 40, height: 70, directionX: -1, directionY: 1, speedX: 1, speedY: 3};
+let object_2 = {x: 0, y: 0, width: 40, height: 70, directionX: 1, directionY: -1, speedX: 1, speedY: 3};
 //Initializing my canvas parameters and variables.
 let canvas_2 = {x: 800, y: 600}
 //Function setup section.
@@ -10,19 +10,19 @@ function setup() {
 function draw() {
     //Adding background color.
     background('black');
+    //Drawing two objects.
+    rect(object_1.x + 10, object_1.y + 300, object_1.width, object_1.height);
+    rect(object_2.x + 760, object_2.y + 300, object_2.width, object_2.height);
     //Motion
-    movedown(object_1, object_2);
-    moveup(object_1, object_2);
+    movedown();
+    moveup();
 }
 //Setting up the functions to move:
 function moveup() {
     if(keyIsDown(UP_ARROW)) {
-        //Drawing objects.
-        rect(object_1.x, object_1.y + 200, object_1.width, object_1.height);
-        rect(object_2.x, object_2.y + 200, object_2.width, object_2.height);
         //Motion.
-        object_1.y = (object_1.speedY * object_1.directionY);
-        object_2.y = (object_2.speedY * object_2.directionY);
+        object_1.y = object_1.y + (object_1.speedY * object_1.directionY);
+        object_2.y = object_2.y + (object_2.speedY * object_2.directionY);
         //Setting up change in direction.
         object_1.directionY = object_1.directionY * -1;
         object_2.directionY = object_2.directionY * -1;
@@ -30,12 +30,9 @@ function moveup() {
 }
 function movedown() {
     if(keyIsDown(DOWN_ARROW)) {
-        //Drawing objects.
-        rect(object_1.x, object_1.y + 200, object_1.width, object_1.height);
-        rect(object_2.x, object_2.y + 200, object_2.width, object_2.height);
         //Motion.
-        object_1.y = (object_1.speedY * object_1.directionY);
-        object_2.y = (object_2.speedY * object_2.directionY);
+        object_1.y = object_1.y + (object_1.speedY * object_1.directionY);
+        object_2.y = object_2.y + (object_2.speedY * object_2.directionY);
         //Direction change.
         object_1.directionY = object_1.directionY*-1;
         object_2.directionY = object_2.directionY*-1;
