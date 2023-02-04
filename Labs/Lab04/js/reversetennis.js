6e066438209012388eb2d6e82b7bdb2b9ef20a60
MHeggy <mhegedui@iu.edu>
2023-02-04 12:36:17 -0500

    big changes to reversetennis.js

--- Labs/Lab04/js/reversetennis.js
+++ Labs/Lab04/js/reversetennis.js
@@ -1,25 +1,43 @@
 //Initializing my objects with their parameters.
-let object_1 = {x: 40, y: 100, width: 40, height: 70, color: 'red', directionX: -1, directionY: 1, speedX: 1, speedY: 1};
-let object_2 = {x: 760, y: 100, width: 40, height: 70, color: 'red'};
+let object_1 = {x: 40, y: 100, width: 40, height: 70, color: 'red', directionX: -1, directionY: 1, speedX: 1, speedY: 3};
+let object_2 = {x: 760, y: 100, width: 40, height: 70, color: 'red', directionX: 1, directionY: -1, speedX: 1, speedY: 3};
 //Initializing my canvas parameters and variables.
 let canvas_2 = {x: 800, y: 600}
 //Function setup section.
 function setup() {
     createCanvas(canvas_2.x, canvas_2.y);
-    fill('black');
+}
+function draw() {
+    //Adding background color.
+    background('black');
+    //Motion
+    movedown(object_1, object_2);
+    moveup(object_1, object_2);
 }
 //Setting up the functions to move:
-function moveup(object_1) {
+function moveup() {
     if(keyIsDown(UP_ARROW)) {
-        //Setting up motion.
-        object_1.x = (object_1.speedY * object_1.directionX)
+        //Drawing objects.
+        rect(object_1.x, object_1.y + 200, object_1.width, object_1.height);
+        rect(object_2.x, object_2.y + 200, object_2.width, object_2.height);
+        //Motion.
+        object_1.y = (object_1.speedY * object_1.directionY);
+        object_2.y = (object_2.speedY * object_2.directionY);
+        //Setting up change in direction.
+        object_1.directionY = object_1.directionY * -1;
+        object_2.directionY = object_2.directionY * -1;
     }
 }
 function movedown() {
     if(keyIsDown(DOWN_ARROW)) {
-
+        //Drawing objects.
+        rect(object_1.x, object_1.y + 200, object_1.width, object_1.height);
+        rect(object_2.x, object_2.y + 200, object_2.width, object_2.height);
+        //Motion.
+        object_1.y = (object_1.speedY * object_1.directionY);
+        object_2.y = (object_2.speedY * object_2.directionY);
+        //Direction change.
+        object_1.directionY = object_1.directionY*-1;
+        object_2.directionY = object_2.directionY*-1;
     }
-}
-function draw() {
-    rect(object_1.)
 }
