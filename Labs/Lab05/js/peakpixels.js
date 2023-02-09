a6b5b9fddf820c08ceed727525770ee8714f8989
MHeggy <mhegedui@iu.edu>
2023-02-09 13:07:13 -0500

    Lab05 changes

---
+++ Labs/Lab05/js/peakpixels.js
@@ -0,0 +1,6 @@
+//Initializing Variables
+let div_1 = {height: 100, width: 100, color_background: 'green'}
+//Now setting up the height and width of the div and background color.
+document.getElementById("peakPixels").width = div_1.width;
+document.getElementById("peakPixels").height = div_1.height;
+document.getElementById("peakPixels").color = div_1.color_background;
