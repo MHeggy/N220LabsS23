d5a2e15e1237416b6abbe30fc3bc8b564b6d972e
MHeggy <mhegedui@iu.edu>
2023-02-14 12:36:50 -0500

    big changes to lab 5

---
+++ Labs/Lab05/js/McDivins.js
@@ -0,0 +1,10 @@
+//Initializing variables.
+let d = document.getElementById("McDiv");
+let text = "Mc";
+d.style.width = 100 + "px";
+d.style.height = 100 + "px";
+d.innerHTML = text;
+
+function McDivin() {
+    d.appendChild(d.textContent += "div");
+}
---
+++ Labs/Lab05/js/overandout.js
@@ -0,0 +1,13 @@
+//Initializing variable d for the div.
+let d = document.getElementById("OverandOut");
+//Changing the style of the div to fit parameters of the assignment.
+d.style.width = 100 + "px";
+d.style.height = 100 + "px";
+d.style.backgroundColor = "blue";
+//Initializing color change function.
+function changeColor() {
+    d.style.backgroundColor = "black";
+}
+function changeBack() {
+    d.style.backgroundColor = "blue";
+}
