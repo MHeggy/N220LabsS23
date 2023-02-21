36c1c9a3f52043cb209c32f14300a02a3aeeb917
MHeggy <mhegedui@iu.edu>
2023-02-21 14:20:11 -0500

    beepbop.js updated and fully commented

--- Labs/Lab06/js/beepbop.js
+++ Labs/Lab06/js/beepbop.js
@@ -1,9 +1,12 @@
+//Setting up an array that has the max and min values for my randomInt function.
 let randomNumArray = [1, 25];
+//Setting up the randomInt() function so that I am able to get a random integer between 1 and 25.
 function randomInt(min, max) {
     return Math.floor(Math.random() * (randomNumArray[1]) + randomNumArray[0]);
 }
+//Initialzing the randomNum variable.
 let randomNum = randomInt(1,25); //returns random integer between 1 and 25.
-console.log(randomNum);
+//Setting up div from HTML file to display the number.
 let div = document.getElementById("beepbop");
 //Setting up the loop to display the randomNum and beep, bop, or beepbop.
 for(i = 1; i <= 25; i++) {
