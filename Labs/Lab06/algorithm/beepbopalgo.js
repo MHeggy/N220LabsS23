b74af669594c6a12e4a20d5d1a569a3bda9ac0f9
MHeggy <mhegedui@iu.edu>
2023-02-21 12:11:38 -0500

    beepbopalgo fixed

--- Labs/Lab06/algorithm/beepbopalgo.js
+++ Labs/Lab06/algorithm/beepbopalgo.js
@@ -4,3 +4,18 @@
 //When the iterand is divisible by 3, beep will be written onto the screen.
 //When the iterand is divisible by 5, the word bop will be displayed instead.
 //And when the iterand is divisible by both 3 and 5, the word beepbop will be written onto the screen.
+//This set of code is rather challenging in my opinion.
+//HTML Code:
+//<div id="beepbop"></div>
+//JS Code:
+//let beepbopdiv = document.getElementById("beepbop");
+//function randomInt(min, max) {
+    //Math.floor(Math.random() * 25) + 1;
+//}
+//let randomNum = randomInt(1, 25);
+//for(i = 1; i <= 25, i++) {
+    //if(randomNum == 3,6,9,12,18,21,24) {
+        //beepbopdiv.innerHTML = randomNum + "beep";
+    //}
+//then I will continue if and else if statements to complete the rest of the loop to provide me with the desired number and beep or bop.
+//}
