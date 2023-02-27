120ef9a6b59fb4f08680c54330a523912cef1189
MHeggy <mhegedui@iu.edu>
2023-02-27 13:01:24 -0500

    More changes to Midterm Review Exercises

---
+++ Labs/Midterm Review Exercises/js/RPS.js
@@ -0,0 +1,2 @@
+let RPGS = [document.getElementById("rock"), document.getElementById("paper"), document.getElementById("scissors"), document.getElementById("guard")];
+let score = document.getElementById("scorecard");
--- Labs/Midterm Review Exercises/js/UINavigationHighlighter.js
+++ Labs/Midterm Review Exercises/js/UINavigationHighlighter.js
@@ -11,35 +11,94 @@ divs[5].innerHTML = "This is Div 6";
 divs[0].style.position = "absolute";
 divs[0].style.top = 0 + "px";
 divs[0].style.border = "solid";
+divs[0].style.height = 100 + "px";
+divs[0].style.width = 100 + "px";
 //Positioning d2.
 divs[1].style.position = "absolute";
 divs[1].style.top = 0 + "px";
 divs[1].style.left = 200 + "px";
 divs[1].style.border = "solid";
+divs[1].style.height = 100 + "px";
+divs[1].style.width = 100 + "px";
 //Position d3.
 divs[2].style.position = "absolute";
 divs[2].style.top = 0 + "px";
 divs[2].style.left = 400 + "px";
 divs[2].style.border = "solid";
+divs[2].style.height = 100 + "px";
+divs[2].style.width = 100 + "px";
 //Position d4.
 divs[3].style.position = "absolute";
 divs[3].style.top = 0 + "px";
 divs[3].style.left = 600 + "px";
 divs[3].style.border = "solid";
+divs[3].style.height = 100 + "px";
+divs[3].style.width = 100 + "px";
 //Position d5.
 divs[4].style.position = "absolute";
 divs[4].style.top = 0 + "px";
 divs[4].style.left = 800 + "px";
 divs[4].style.border = "solid";
+divs[4].style.height = 100 + "px";
+divs[4].style.width = 100 + "px";
 //Position d6.
 divs[5].style.position = "absolute";
 divs[5].style.top = 0 + "px";
 divs[5].style.left = 1000 + "px";
 divs[5].style.border = "solid";
+divs[5].style.height = 100 + "px";
+divs[5].style.width = 100 + "px";
 //Setting up the function for divs.
-function changeDivs() {
-    if(divs[0].onclick == "true") {
-        divs[0].innerHTML = "You clicked on Div 1!";
-        divs[1,2,3,4,5].innerHTML = "";
-    }
+function changeDiv1() {
+    divs[0].style.backgroundColor = "yellow";
+    divs[0].innerHTML = "You have clicked on Div 1!";
+    divs[2].innerHTML = "This is Div 1";
+    divs[1].innerHTML = "";
+    divs[3].innerHTML = "";
+    divs[4].innerHTML = "";
+    divs[5].innerHTML = "";
+}
+function changeDiv2() {
+    divs[1].style.backgroundColor = "yellow";
+    divs[1].innerHTML = "You have clicked on Div 2!";
+    divs[2].innerHTML = "This is Div 2";
+    divs[0].innerHTML = "";
+    divs[3].innerHTML = "";
+    divs[4].innerHTML = "";
+    divs[5].innerHTML = "";
+}
+function changeDiv3() {
+    divs[2].style.backgroundColor = "yellow";
+    divs[2].innerHTML = "You have clicked on Div 3!";
+    divs[3].innerHTML = "This is Div 3";
+    divs[0].innerHTML = "";
+    divs[1].innerHTML = "";
+    divs[4].innerHTML = "";
+    divs[5].innerHTML = "";
+}
+function changeDiv4() {
+    divs[3].style.backgroundColor = "yellow";
+    divs[3].innerHTML = "You have clicked on Div 4!";
+    divs[1].innerHTML = "This is Div 4";
+    divs[0].innerHTML = "";
+    divs[2].innerHTML = "";
+    divs[4].innerHTML = "";
+    divs[5].innerHTML = "";
+}
+function changeDiv5() {
+    divs[4].style.backgroundColor = "yellow";
+    divs[4].innerHTML = "You have clicked on Div 5!";
+    divs[2].innerHTML = "This is Div 5";
+    divs[0].innerHTML = "";
+    divs[3].innerHTML = "";
+    divs[5].innerHTML = "";
+}
+function changeDiv6() {
+    divs[5].style.backgroundColor = "yellow";
+    divs[5].innerHTML = "You have clicked on Div 6!";
+    divs[2].innerHTML = "This is Div 6";
+    divs[0].innerHTML = "";
+    divs[3].innerHTML = "";
+    divs[4].innerHTML = "";
+    divs[1].innerHTML = "";
 }
