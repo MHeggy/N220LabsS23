8d649fd3c933f603bb83db7df7545888777de98e
MHeggy <mhegedui@iu.edu>
2023-03-21 13:05:19 -0400

    Week9InputsExamples

---
+++ Examples/Week 9/week9inputs.js
@@ -0,0 +1,11 @@
+//We are using inputs today, which I already have experience using luckily!
+//<input id="userName" />
+//They are an HTML tag so it makes the code quite simple.
+//We can use them quite often and for a littany of different reasons and coding fun.
+//Placeholder also is a nice thing that inputs allow us to use so generously.
+//to access inputs we use document.querySelector and the ID of the element.
+//HTML Code: <input id="txtName">
+//JS Code: let txt = document.getElementById("txtName");
+//We don't use innerHTML for inputs rather we use "value".
+//txt.innerHTML == WRONG
+//txt.value = CORRECT
